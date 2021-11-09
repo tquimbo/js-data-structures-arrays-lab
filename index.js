224af8d18c2c7e0c47fb4401d52095189abb61fc
Tim Quimbo <tquimbo1@gmail.com>
2021-11-09 01:29:41 -0500

    commit tree 09c34ab05ea3443c3883163a11c7991e304b8479

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"]
+append.push('Odie');
+append;
+
+
+const prepend = ["Milo", "Otis", "Garfield"]
+prepend.unshift('Odie');
+append;
+
+const removeLast = ["Milo", "Otis", "Garfield"]
+removeLast.pop();
+removeLast;
+
+const removeFirst = ["Milo", "Otis", "Garfield"]
+removeFirst.shift();
+removeFirst;
